d6ffb404980693160fdc07cdf210570c588f3321
Pete Falco <falcoventures@gmail.com>
2024-06-22 17:54:02 -0400

    t

--- public/php/api.php
+++ public/php/api.php
@@ -6,7 +6,14 @@ Template Name: Serve
 $txt1 = "Learn PHP";
 $txt2 = "W3Schools.com";
 
+
+// From URL to get webpage contents.
+$code = $_GET['code']
+
 echo "<h2>$txt1</h2>";
+
+// echo "link = ";
+echo "<p>{\"code\":\"" + $code + "\"}</p>";
 echo "<p>Study PHP at $txt2</p>";
 
 
@@ -47,10 +54,6 @@ echo "<p>Study PHP at $txt2</p>";
 //     }
 
 
-// From URL to get webpage contents.
-$code = $_GET['code']
-echo "link = ";
-echo $code;
 // $data = array("code" => $code , "grant_type" => "authorization_code");                                                                    
 // $data_string = json_encode($data);     
 // $result = CurlSendPostRequest("https://login.eveonline.com/oauth/token", $data_string);
