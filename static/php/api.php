a3e30fe1eeca581f512443f25512991b740e124e
Pete Falco <falcoventures@gmail.com>
2024-06-22 19:19:51 -0400

    get code add fn

--- static/php/api.php
+++ static/php/api.php
@@ -3,24 +3,57 @@
 Template Name: Serve
 */
 
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
+// $txt1 = "Learn PHP";
+// $txt2 = "W3Schools.com";
+
+// echo "<h2>$txt1</h2>";
+// echo "<p>Study PHP at $txt2</p>";
+
+
+ 
+function CurlSendPostRequest($url,$request)
+    {
+        $authentication = base64_encode("5fe7b21736e748c6a78d9e4f98ff536e:5e0tEfn1tNwFPvEz4EEcXcJIpSngdGQBc3cbdOgU");
+
+        $ch = curl_init($url);
+        $options = array(
+                CURLOPT_RETURNTRANSFER => true,         // return web page
+                CURLOPT_HEADER         => false,        // don't return headers
+                CURLOPT_FOLLOWLOCATION => false,         // follow redirects
+               // CURLOPT_ENCODING       => "utf-8",           // handle all encodings
+                CURLOPT_AUTOREFERER    => true,         // set referer on redirect
+                CURLOPT_CONNECTTIMEOUT => 20,          // timeout on connect
+                CURLOPT_TIMEOUT        => 20,          // timeout on response
+                CURLOPT_POST            => 1,            // i am sending post data
+                CURLOPT_POSTFIELDS     => $request,    // this are my post vars
+                CURLOPT_SSL_VERIFYHOST => 0,            // don't verify ssl
+                CURLOPT_SSL_VERIFYPEER => false,        //
+                CURLOPT_VERBOSE        => 1,
+                CURLOPT_HTTPHEADER     => array(
+                    "Authorization: Basic $authentication",
+                    "Content-Type: application/json"
+                )
+
+        );
+
+        curl_setopt_array($ch,$options);
+        $data = curl_exec($ch);
+        $curl_errno = curl_errno($ch);
+        $curl_error = curl_error($ch);
+        //echo $curl_errno;
+        //echo $curl_error;
+        curl_close($ch);
+        return $data;
+    }
 
 
 // From URL to get webpage contents.
 $code = $_GET['code'];
-// $code = "Hello";
-echo "<h2>$txt1</h2>";
+echo $code;
 
+echo "<h2>$code</h2>";
 // $data = array("code" => $code , "grant_type" => "authorization_code");                                                                    
-$data_string = $code; //json_encode($data);     
-$entityBody = file_get_contents('php://input');
-// echo "link = ";
-echo "<p>Body $entityBody</p>";
-echo "<p>Study PHP at $data_string</p>";
-echo "<p>Study PHP at $txt2</p>";
-
- 
+// $data_string = json_encode($data);     
 // $result = CurlSendPostRequest("https://login.eveonline.com/oauth/token", $data_string);
 // echo $result;
 
