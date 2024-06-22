5169b9b23fcd8a62ad7fcbc4ff40bffcaac68ef4
Pete Falco <falcoventures@gmail.com>
2024-06-22 19:10:14 -0400

    test

--- static/php/api.php
+++ static/php/api.php
@@ -12,12 +12,12 @@ $txt2 = "W3Schools.com";
 $code = "Hello"
 echo "<h2>$txt1</h2>";
 
-$data = array("code" => $code , "grant_type" => "authorization_code");                                                                    
-$data_string = json_encode($data);     
+// $data = array("code" => $code , "grant_type" => "authorization_code");                                                                    
+$data_string = $code; //json_encode($data);     
 // $entityBody = file_get_contents('php://input');
 // echo "link = ";
 echo "<p>Body $entityBody</p>";
-echo "<p>Study PHP at $data_string </p>";
+echo "<p>Study PHP at $data_string</p>";
 echo "<p>Study PHP at $txt2</p>";
 
  
